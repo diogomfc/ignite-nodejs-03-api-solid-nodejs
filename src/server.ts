dbd0398f15910c19e8d01c54daf41a3124612c77
Diogo Silva <diogomfc@hotmail.com>
2023-09-18 09:02:04 -0300

    feat: Estrutura do projeto - Carregando variáveis ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import { app } from "./app"
+import {env } from './env'
 
 app.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
 
 app.listen({
-  port: 3333,
+  port: env.PORT,
   host: '0.0.0.0'
 }).then(() => {
-  console.log('🚀 HTTP Server Running on port 3333')
+  console.log('🚀 HTTP Server Running!')
 })
 
