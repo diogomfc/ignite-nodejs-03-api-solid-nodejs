5aa0fa878d5b0f42b70ff2394907b9b08e8183ae
Diogo Silva <diogomfc@hotmail.com>
2023-09-19 18:59:11 -0300

    feat: Implementando casos de uso - TDD & Mocking

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,9 +1,8 @@
 
-import {expect, describe, it, beforeEach} from 'vitest';
+import {expect, describe, it, beforeEach, vi, afterEach} from 'vitest';
 
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
 import { CheckInUseCase } from './check-in';
-
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: CheckInUseCase;
@@ -14,18 +13,39 @@ describe('Check-in Use Case', () => {
     beforeEach(() => {
         checkInsRepository = new InMemoryCheckInsRepository();
         sut = new CheckInUseCase(checkInsRepository);
+
+        vi.useFakeTimers();
     }); 
+
+    afterEach(() => {
+        vi.useRealTimers();
+    });
 
 
     it('should be able to check in', async () => {
-
         const {checkIn} = await sut.execute({
             gymId: 'user-gym-id',
             userId: 'user-id',
         });
-         
+
         await expect(checkIn.id).toEqual(expect.any(String));
 
+    });
+
+    it('should be able to check in twice in the same day', async () => {
+        vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+        
+        await sut.execute({
+            gymId: 'user-gym-id',
+            userId: 'user-id',
+        });
+
+        await expect(() => 
+            sut.execute({
+                gymId: 'user-gym-id',
+                userId: 'user-id',
+            }),
+        ).rejects.toBeInstanceOf(Error);
     });
  
 });
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -15,6 +15,13 @@ export class CheckInUseCase{
     constructor(private checkInsRepository: CheckInsRepository) {}
 
     async execute({userId, gymId}:CheckInUseCaseRequest):Promise<CheckInUseCaseResponse> {
+        
+        const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(userId, new Date());
+
+        if (checkInOnSameDay){
+            throw new Error('Check-in already done today');
+        }
+      
         const checkIn = await this.checkInsRepository.create({
             user_id: userId,
             gym_id: gymId,
